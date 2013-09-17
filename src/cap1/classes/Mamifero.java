b912c37063be4d253848a0d1fd439a348fe93bfc
jardel <jardel@jardel.com>
2013-09-17 11:46:15 -0300

    Classes

---
+++ src/cap1/classes/Mamifero.java
@@ -0,0 +1,4 @@
+package cap1.classes;
+
+class Mamifero {
+}
---
+++ src/cap1/classes/cont/Cachorro.java
@@ -0,0 +1,18 @@
+package cap1.classes.cont;
+
+import cap1.classes.Mamifero;
+
+public class Cachorro extends Mamifero{
+	
+	/**
+	
+	Essa class nao compila devido ao fato que a classe Mamifero esteja default ou seja,
+	sem o tipo public, a classe default so pode ser visto pelo proprio package.
+	
+	A solução seria: Declarar a class Mamifero como public class Mamifero
+	assim ela estaria disponivel para todas os package do projeto.
+	
+	**/
+	
+	
+}
